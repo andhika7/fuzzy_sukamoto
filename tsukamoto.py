738f2f6523f2dc30ba82a4719ba22d758134c2e1
Andhika <andhika.theinnovator@gmail.com>
2023-10-17 14:20:03 +0700

    revisi metode tsukamoto

--- tsukamoto.py
+++ tsukamoto.py
@@ -120,111 +120,231 @@ print(f"keanggotaan agak terang: {h}")
 i = u_terang(float(inputan_cahaya))
 print(f"keanggotaan terang: {i}")
 
+def prod_bertambah(rata2, predikat):
+    if rata2>=142 and rata2<148:
+        x=(predikat*6)+142
+    elif rata2>=148 and rata2<155:
+        x=155-(predikat*7)
+    else:
+        x=0
+    return x
 
-jml_produk=[]
+def prod_agakBertambah(rata2, predikat):
+    if rata2>=136 and rata2<142:
+        x=(predikat*6)+136
+    elif rata2>=142 and rata2<148:
+        x=148-(predikat*6)
+    else:
+        x=0
+    return x
+
+def prod_berkurang(rata2, predikat):
+    if rata2>=130 and rata2<136:
+        x=(predikat*6)+130
+    elif rata2>=136 and rata2<142:
+        x=142-(predikat*6)
+    else:
+        x=0
+    return x
+    
+jml_produk=[148.0, 150.9, 146.5, 143.1, 146.53, 142.73, 136.73, 140.77, 135.97, 149.73, 153.27, 152.13, 148.00, 150.63, 147.63, 141.47, 145.67, 140.20, 142.10, 146.53, 142.17, 138.70, 141.40, 138.30, 133.33, 138.53, 133.77]
+
+himpunan_produk=[]
 a_predikat=[]
 
+"""
+berkurang  agak   bertambah
+          btambah  
+      /\    /\    /\
+     /  \  /  \  /  \
+    /    \/    \/    \
+   /     /\    /\     \
+  /     /  \  /  \     \
+ /     /    \/    \     \
+130   136   142   148   155
+"""
 # ***************************** INFERENCE ************************
-# [1] if suhu RENDAH, kebisingan TENANG, cahaya REDUP => JML PRODUK = 148.0
-a_predikat.append(min(a,d,g))
-jml_produk.append(148.0)
-# [2] if suhu RENDAH, kebisingan TENANG, cahaya AGAK TERANG => JML PRODUK = 150.9
-a_predikat.append(min(a,d,h))
-jml_produk.append(150.9)
-# [3] if suhu RENDAH, kebisingan TENANG, cahaya TERANG => JML PRODUK = 146.5
-a_predikat.append(min(a,d,i))
-jml_produk.append(146.5)
-# [4] if suhu RENDAH, kebisingan AGAK BISING, cahaya REDUP => JML PRODUK = 143.1
-a_predikat.append(min(a,e,g))
-jml_produk.append(143.1)
-# [5] if suhu RENDAH, kebisingan AGAK BISING, cahaya AGAK TERANG => JML PRODUK = 146.53
-a_predikat.append(min(a,e,h))
-jml_produk.append(146.53)
-# [6] if suhu RENDAH, kebisingan AGAK BISING, cahaya TERANG => JML PRODUK = 142.73
-a_predikat.append(min(a,e,i))
-jml_produk.append(142.73)
-# [7] if suhu RENDAH, kebisingan BISING, cahaya REDUP => JML PRODUK = 136.73
-a_predikat.append(min(a,f,g))
-jml_produk.append(136.73)
-# [8] if suhu RENDAH, kebisingan BISING, cahaya AGAK TERANG => JML PRODUK = 140.77
-a_predikat.append(min(a,f,h))
-jml_produk.append(140.77)
-# [9] if suhu RENDAH, kebisingan BISING, cahaya TERANG => JML PRODUK = 135.97
-a_predikat.append(min(a,f,i))
-jml_produk.append(135.97)
+# [1] if suhu RENDAH, kebisingan TENANG, cahaya REDUP => JML PRODUK = 148.0 (BERTAMBAH)
+alpha = min(a,d,g)
+a_predikat.append(alpha)
+rata = jml_produk[0]
+R1 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R1)
+# [2] if suhu RENDAH, kebisingan TENANG, cahaya AGAK TERANG => JML PRODUK = 150.9 (BERTAMBAH)
+alpha = min(a,d,h)
+a_predikat.append(alpha)
+rata = jml_produk[1]
+R2 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R2)
+# [3] if suhu RENDAH, kebisingan TENANG, cahaya TERANG => JML PRODUK = 146.5 (AGAK BERTAMBAH)
+alpha = (min(a,d,i))
+a_predikat.append(alpha)
+rata = jml_produk[2]
+R3 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R3)
+# [4] if suhu RENDAH, kebisingan AGAK BISING, cahaya REDUP => JML PRODUK = 143.1 (AGAK BERTAMBAH)
+alpha = (min(a,e,g))
+a_predikat.append(alpha)
+rata = jml_produk[3]
+R4 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R4)
+# [5] if suhu RENDAH, kebisingan AGAK BISING, cahaya AGAK TERANG => JML PRODUK = 146.53 (AGAK BERTAMBAH)
+alpha = (min(a,e,h))
+a_predikat.append(alpha)
+rata = jml_produk[4]
+R5 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R5)
+# [6] if suhu RENDAH, kebisingan AGAK BISING, cahaya TERANG => JML PRODUK = 142.73 (AGAK BERTAMBAH)
+alpha = (min(a,e,i))
+a_predikat.append(alpha)
+rata = jml_produk[5]
+R6 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R6)
+# [7] if suhu RENDAH, kebisingan BISING, cahaya REDUP => JML PRODUK = 136.73 (BERKURANG)
+alpha = (min(a,f,g))
+a_predikat.append(alpha)
+rata = jml_produk[6]
+R7 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R7)
+# [8] if suhu RENDAH, kebisingan BISING, cahaya AGAK TERANG => JML PRODUK = 140.77 (BERKURANG)
+alpha = (min(a,f,h))
+a_predikat.append(alpha)
+rata = jml_produk[7]
+R8 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R8)
+# [9] if suhu RENDAH, kebisingan BISING, cahaya TERANG => JML PRODUK = 135.97 (BERKURANG)
+alpha = (min(a,f,i))
+a_predikat.append(alpha)
+rata = jml_produk[8]
+R9 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R9)
 
-# [10] if suhu NORMAL, kebisingan TENANG, cahaya REDUP => JML PRODUK = 149.73
-a_predikat.append(min(b,d,g))
-jml_produk.append(149.73)
-# [11] if suhu NORMAL, kebisingan TENANG, cahaya AGAK TERANG => JML PRODUK = 153.27
-a_predikat.append(min(b,d,h))
-jml_produk.append(153.27)
-# [12] if suhu NORMAL, kebisingan TENANG, cahaya TERANG => JML PRODUK = 152.13
-a_predikat.append(min(b,d,i))
-jml_produk.append(152.13)
-# [13] if suhu NORMAL, kebisingan AGAK BISING, cahaya REDUP => JML PRODUK = 148
-a_predikat.append(min(b,e,g))
-jml_produk.append(148)
-# [14] if suhu NORMAL, kebisingan AGAK BISING, cahaya AGAK TERANG => JML PRODUK = 150.63
-a_predikat.append(min(b,e,h))
-jml_produk.append(150.63)
-# [15] if suhu NORMAL, kebisingan AGAK BISING, cahaya TERANG => JML PRODUK = 147.63
-a_predikat.append(min(b,e,i))
-jml_produk.append(147.63)
-# [16] if suhu NORMAL, kebisingan BISING, cahaya REDUP => JML PRODUK = 141.47
-a_predikat.append(min(b,f,g))
-jml_produk.append(141.47)
-# [17] if suhu NORMAL, kebisingan BISING, cahaya AGAK TERANG => JML PRODUK = 145.67
-a_predikat.append(min(b,f,h))
-jml_produk.append(145.67)
-# [18] if suhu NORMAL, kebisingan BISING, cahaya TERANG => JML PRODUK = 140.2
-a_predikat.append(min(b,f,i))
-jml_produk.append(140.2)
+# [10] if suhu NORMAL, kebisingan TENANG, cahaya REDUP => JML PRODUK = 149.73 (BERTAMBAH)
+alpha = (min(b,d,g))
+a_predikat.append(alpha)
+rata = jml_produk[9]
+R10 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R10)
+# [11] if suhu NORMAL, kebisingan TENANG, cahaya AGAK TERANG => JML PRODUK = 153.27 (BERTAMBAH)
+alpha = (min(b,d,h))
+a_predikat.append(alpha)
+rata = jml_produk[10]
+R11 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R11)
+# [12] if suhu NORMAL, kebisingan TENANG, cahaya TERANG => JML PRODUK = 152.13 (BERTAMBAH)
+alpha = (min(b,d,i))
+a_predikat.append(alpha)
+rata = jml_produk[11]
+R12 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R12)
+# [13] if suhu NORMAL, kebisingan AGAK BISING, cahaya REDUP => JML PRODUK = 148 (BERTAMBAH)
+alpha = (min(b,e,g))
+a_predikat.append(alpha)
+rata = jml_produk[12]
+R13 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R13)
+# [14] if suhu NORMAL, kebisingan AGAK BISING, cahaya AGAK TERANG => JML PRODUK = 150.63 (BERTAMBAH)
+alpha = (min(b,e,h))
+a_predikat.append(alpha)
+rata = jml_produk[13]
+R14 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R14)
+# [15] if suhu NORMAL, kebisingan AGAK BISING, cahaya TERANG => JML PRODUK = 147.63 (BERTAMBAH)
+alpha = (min(b,e,i))
+a_predikat.append(alpha)
+rata = jml_produk[14]
+R15 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R15)
+# [16] if suhu NORMAL, kebisingan BISING, cahaya REDUP => JML PRODUK = 141.47 (AGAK BERTAMBAH)
+alpha = (min(b,f,g))
+a_predikat.append(alpha)
+rata = jml_produk[15]
+R16 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R16)
+# [17] if suhu NORMAL, kebisingan BISING, cahaya AGAK TERANG => JML PRODUK = 145.67 (AGAK BERTAMBAH)
+alpha = (min(b,f,h))
+a_predikat.append(alpha)
+rata = jml_produk[16]
+R17 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R17)
+# [18] if suhu NORMAL, kebisingan BISING, cahaya TERANG => JML PRODUK = 140.2 (BERKURANG)
+alpha = (min(b,f,i))
+a_predikat.append(alpha)
+rata = jml_produk[17]
+R18 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R18)
 
-# [19] if suhu TINGGI, kebisingan TENANG, cahaya REDUP => JML PRODUK = 142.10
-a_predikat.append(min(c,d,g))
-jml_produk.append(142.10)
-# [20] if suhu TINGGI, kebisingan TENANG, cahaya AGAK TERANG => JML PRODUK = 146.53
-a_predikat.append(min(c,d,h))
-jml_produk.append(146.53)
-# [21] if suhu TINGGI, kebisingan TENANG, cahaya TERANG => JML PRODUK = 142.17
-a_predikat.append(min(c,d,i))
-jml_produk.append(142.17)
-# [22] if suhu TINGGI, kebisingan AGAK BISING, cahaya REDUP => JML PRODUK = 138.7
-a_predikat.append(min(c,e,g))
-jml_produk.append(138.7)
-# [23] if suhu TINGGI, kebisingan AGAK BISING, cahaya AGAK TERANG => JML PRODUK = 141.4
-a_predikat.append(min(c,e,h))
-jml_produk.append(141.4)
-# [24] if suhu TINGGI, kebisingan AGAK BISING, cahaya TERANG => JML PRODUK = 138.3
-a_predikat.append(min(c,e,i))
-jml_produk.append(138.3)
-# [25] if suhu TINGGI, kebisingan BISING, cahaya REDUP => JML PRODUK = 133.33
-a_predikat.append(min(c,f,g))
-jml_produk.append(133.33)
-# [26] if suhu TINGGI, kebisingan BISING, cahaya AGAK TERANG => JML PRODUK = 138.33
-a_predikat.append(min(c,f,h))
-jml_produk.append(138.33)
-# [27] if suhu TINGGI, kebisingan BISING, cahaya TERANG => JML PRODUK = 133.77
-a_predikat.append(min(c,f,i))
-jml_produk.append(133.77)
-
+# [19] if suhu TINGGI, kebisingan TENANG, cahaya REDUP => JML PRODUK = 142.10 (AGAK BERTAMBAH)
+alpha = (min(c,d,g))
+a_predikat.append(alpha)
+rata = jml_produk[18]
+R19 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R19)
+# [20] if suhu TINGGI, kebisingan TENANG, cahaya AGAK TERANG => JML PRODUK = 146.53 (BERTAMBAH)
+alpha = (min(c,d,h))
+a_predikat.append(alpha)
+rata = jml_produk[19]
+R20 = prod_bertambah(rata,alpha)
+himpunan_produk.append(R20)
+# [21] if suhu TINGGI, kebisingan TENANG, cahaya TERANG => JML PRODUK = 142.17 (AGAK BERTAMBAH)
+alpha = (min(c,d,i))
+a_predikat.append(alpha)
+rata = jml_produk[20]
+R21 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R21)
+# [22] if suhu TINGGI, kebisingan AGAK BISING, cahaya REDUP => JML PRODUK = 138.7 (BERKURANG)
+alpha = (min(c,e,g))
+a_predikat.append(alpha)
+rata = jml_produk[21]
+R22 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R22)
+# [23] if suhu TINGGI, kebisingan AGAK BISING, cahaya AGAK TERANG => JML PRODUK = 141.4 (AGAK BERTAMBAH)
+alpha = (min(c,e,h))
+a_predikat.append(alpha)
+rata = jml_produk[22]
+R23 = prod_agakBertambah(rata,alpha)
+himpunan_produk.append(R23)
+# [24] if suhu TINGGI, kebisingan AGAK BISING, cahaya TERANG => JML PRODUK = 138.3 (BERKURANG)
+alpha = (min(c,e,i))
+a_predikat.append(alpha)
+rata = jml_produk[23]
+R24 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R24)
+# [25] if suhu TINGGI, kebisingan BISING, cahaya REDUP => JML PRODUK = 133.33 (BERKURANG)
+alpha = (min(c,f,g))
+a_predikat.append(alpha)
+rata = jml_produk[24]
+R25 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R25)
+# [26] if suhu TINGGI, kebisingan BISING, cahaya AGAK TERANG => JML PRODUK = 138.33 (BERKURANG)
+alpha = (min(c,f,h))
+a_predikat.append(alpha)
+rata = jml_produk[25]
+R26 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R26)
+# [27] if suhu TINGGI, kebisingan BISING, cahaya TERANG => JML PRODUK = 133.77 (BERKURANG)
+alpha = (min(c,f,i))
+a_predikat.append(alpha)
+rata = jml_produk[26]
+R27 = prod_berkurang(rata,alpha)
+himpunan_produk.append(R27)
 
 # ********************** DEFUZZYFIKASI ********************
 print("\n")
-a_pred_x_prod = 0
-total_a_pred = 0
-z = 0
+a_predikat_x_produk = 0
+total_a_predikat = 0
+z = 0 
 
 for i in range(27):
-    a_pred_x_prod += a_predikat[i]*jml_produk[i]
-    total_a_pred += a_predikat[i]
-print(a_pred_x_prod)
-print(total_a_pred)
+    a_predikat_x_produk += a_predikat[i]*himpunan_produk[i]
+    total_a_predikat += a_predikat[i]
+print(f"total a_pred*produk: {a_predikat_x_produk}")
+print(f"total a_predikat: {total_a_predikat}")
 
-if total_a_pred == 0:
+# ********************** CRISP OUTPUT ********************
+if total_a_predikat == 0:
     print("nilai fuzzy undefined")
 else:
-    z = a_pred_x_prod / total_a_pred
+    z = a_predikat_x_produk / total_a_predikat
     z_2dig = "{:.2f}".format(z)
     print(f"fuzzy out: {z_2dig}")
